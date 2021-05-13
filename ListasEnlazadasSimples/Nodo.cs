58d6bdf03190b2c4d557c4156de0764f370cbd89
Juan Santos <juanrscaba@gmail.com>
2021-05-12 22:04:12 -0400

    codigo limpioAdd project files.

---
+++ ListasEnlazadasSimples/Nodo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListasEnlazadasSimples
+{
+    public class Nodo<T>
+    {
+		private T Data;
+		private Nodo<T> _Next;
+		private Nodo<T> _Previous;
+
+		public Nodo(T valor)
+		{
+			this.Data = valor;
+		}
+		public void setNext(Nodo<T> next)
+		{
+			this._Next = next;
+		}
+
+		public void setPrevious(Nodo<T> previous)
+		{
+			this._Previous = previous;
+		}
+		public T getData()
+		{
+			return this.Data;
+		}
+		public Nodo<T> getNext()
+		{
+			return this._Next;
+		}
+		//agrregue nodo previo
+		public Nodo<T> getPrevious()
+		{
+			return this._Previous;
+		}
+	}
+}
---
+++ ListasEnlazadasSimples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListasEnlazadasSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			
+
+			// Arreglo generico
+			int[] arreglo = new int[] { 1, 2, 3 };
+
+			// Ejecución de la lista enlazada circulares
+
+			ListaEnlazadaCircular<string> lista = new ListaEnlazadaCircular<string>();
+			ListaDobleEnlazadaCircular<int> numeros = new ListaDobleEnlazadaCircular<int>();
+
+			numeros.InsertRange(new int[] { 1,2,3,4,5});
+			//  numeros.retornaLista();
+
+			lista.InsertRange(new string[] { "juan","pedro"});
+			lista.retornaLista();
+
+
+
+        }
+	}
+}
